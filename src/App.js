21e3dd5e2f5bda9fc1ea0db414a8534930d8673c
Anwar Hossain <mahedianwar@gmail.com>
2022-08-04 12:06:32 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { storePosition } from './constant/data';
 import { useDistance } from './hook/useDistance';
 import { useGeolocation } from './hook/useGeolocation';
